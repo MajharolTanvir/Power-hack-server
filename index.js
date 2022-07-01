e27c75f3c6fecf467af4060ad640b0e9d64fe576
Majharul tanvir <mdtanvir6070@gmail.com>
2022-07-01 14:50:48 +0600

    data delete route complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -18,17 +18,25 @@ async function run() {
         await client.connect();
         const billCollection = client.db("Power-hack").collection("bills");
 
-        app.get('/bills', async (req, res) => {
+        app.get('/billing-list', async (req, res) => {
             const result = await billCollection.find().toArray()
             res.send(result)
         })
         
-        app.post('/bills', async (req, res) => {
+
+        app.post('/add-billing', async (req, res) => {
             const query = req.body
             const result = await billCollection.insertOne(query)
             res.send(result)
         })
 
+        
+        app.delete('/delete-billing/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await billCollection.deleteOne(filter)
+            res.send(result)
+        })
 
     } finally {
         // await client.close();
